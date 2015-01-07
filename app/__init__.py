8f2837a713190a673021f58f992799038df05f94
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-01-07 11:29:22 -0800

    typo fix; leftover 'main' from app restructuring

--- app/__init__.py
+++ app/__init__.py
@@ -26,7 +26,7 @@ def index():
 
     if request.method == 'GET':
 
-        return render_template('main/index.html')
+        return render_template('index.html')
 
     elif request.method == 'POST':
 
