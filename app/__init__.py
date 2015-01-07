59c759a7e51ebce62f8ddfd4b5dcdca0325c0455
Elijah Hamovitz <elijahhamovitz@gmail.com>
2015-01-06 20:37:22 -0800

    initial commit

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template
+
+def create_app(config_overrides={}):
+    """Create and return an Flask app instance"""
+
+    # create app; load config
+    app = Flask(__name__)
+    app.config.from_object('config')
+    app.config.update(**config_overrides)
+
+    db.init_app(app)
+
+    # error page handlers
+    @app.errorhandler(404)
+    def not_found(error):
+        return render_template('404.html'), 404
+
+    @app.errorhandler(500)
+    def server_error(error):
+        return render_template('500.html'), 500
+
+    # register blueprints
+    from app.main.views import mod as main_module
+
+    app.register_blueprint(main_module)
+
+    return app
---
+++ config.py
@@ -0,0 +1,24 @@
+import os
+_basedir = os.path.abspath(os.path.dirname(__file__))
+
+THREADS_PER_PAGE = 8
+
+DEBUG = False
+
+HOST = '127.0.0.1'
+PORT = 8000
+
+# both SECRET_KEY and CSRF_SESSION_KEY should  be overwridden in
+# config_local
+SECRET_KEY = "some_secret_key"
+CSRF_SESSION_KEY = "some_other_secret_key"
+
+CSRF_ENABLED = True
+
+PRAW_USER_AGENT = "ror/0.0.1 by /u/hamms"
+
+# Local overrides
+try:
+    from config_local import *
+except ImportError as e:
+    pass
